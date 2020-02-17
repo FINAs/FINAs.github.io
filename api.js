f0be065e15236c7051a010ad4bb6b5e0dd6c6b27
cerda <kyzy_0011412@163.com>
2020-02-17 08:26:10 +0800

    update

---
+++ api.js
@@ -0,0 +1,4 @@
+//沪股通，深股通
+'http://push2.eastmoney.com/api/qt/kamt/get?fields1=f1,f3&fields2=f51,f52,f53,f54&cb=cb1';
+//股指数据
+'http://90.push2.eastmoney.com/api/qt/clist/get?cb=cb&pn=1&pz=5&po=1&np=1&fid=&fs=b:MK0010&fields=f2,f4,f5,f6,f14,f15,f16,f17,f18,f20,f25'
