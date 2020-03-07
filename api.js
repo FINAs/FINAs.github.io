85c800226527db2c5f0431f1744a990d7bb53f06
link <gameboystay13@gmail.com>
2020-03-07 12:53:27 +0800

    update

--- api.js
+++ api.js
@@ -2,3 +2,7 @@
 'http://push2.eastmoney.com/api/qt/kamt/get?fields1=f1,f3&fields2=f51,f52,f53,f54&cb=cb1';
 //股指数据
 'http://90.push2.eastmoney.com/api/qt/clist/get?cb=cb&pn=1&pz=5&po=1&np=1&fid=&fs=b:MK0010&fields=f2,f4,f5,f6,f14,f15,f16,f17,f18,f20,f25'
+//股票数据
+'http://push2.eastmoney.com/api/qt/stock/details/get?secid=1.510500&fields1=f4&fields2=f52&pos=-11&cb=j'
+//基金盘中
+'http://fundgz.1234567.com.cn/js/510500.js?rt=1583209671839&callback=a'
